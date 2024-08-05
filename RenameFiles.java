ae806eb6936bfd79ea3bc5f7913c0ebe7e766c4d
semenyutenko <semenyutenko@gmail.com>
2024-08-06 01:56:33 +0300

    First commit newBranch

--- RenameFiles.java
+++ RenameFiles.java
@@ -9,19 +9,23 @@ import java.util.stream.Stream;
 public class RenameFiles {
 
     public static void main(String[] x) throws Exception {
+        //test
         if (x.length < 1) {
             System.err.println("The directory URL hasn't been pointed");
             return;
         }
+        //test2
         Path p = Paths.get(x[0]);
         try (Stream<Path> stream = Files.walk(p)) {
             stream.skip(1).forEach(RenameFiles::renameFile);
         }
+        //test3
     }
 
     private static void renameFile(Path path) {
         if (!Files.isRegularFile(path) || !Files.isReadable(path)) {
             return;
+            //test5
         }
         String fileName = path.getFileName().toString();
         String extension = "";
